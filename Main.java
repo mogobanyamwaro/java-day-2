be9cc5e9939e88f1ec5808b5d3ec11ac12063b4a
Douglas Mogoba Nyamwaro <mogobanyamwaro@gmail.com>
2021-05-21 05:46:55 +0300

    casting

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
 /**
  * Main
  */
+// public class Main {
+
+//     public static void main(String[] args) {
+//         int x = 10 + 3 * 3;
+//         System.out.println(x);
+
+//     }
+// }
+
+/**
+ * Main
+ */
 public class Main {
 
     public static void main(String[] args) {
-        int x = 10 + 3 * 3;
-        System.out.println(x);
+        double z = 1.1;
+        double y = z +2;
+        String x = "1";
+        int p = Integer.parseInt(x) + 3;
+        System.out.println(y);
 
     }
 }
